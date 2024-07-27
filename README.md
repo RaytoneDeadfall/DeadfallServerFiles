e7c649d493ff83aae40275ad164a34ca0cd924e6
J H <jason@greenhell.com>
2024-07-27 13:41:56 -0400

    initial public files import

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# Deadfall-Mission
-Mission files for Deadfall DayZ Terrain Mod
+# Deadfall Server Files
+
+This repo should contain everything needed for running a Deadfall server.  It contains mission folder,
+server profile/config files (found in `extras/`), and CE files for further editing of the economy.
+
+
+## Additional Mods
+
+The following mods and profile config files are used to enrich the overall Deadfall experience.
+
+### Stargate Teleporter (required)
+
+Deadfall has a hard dependency on the Stargate Teleport mod, as some of the terrain is inaccessible without
+teleportation.  In the future, this might be implemented internally, but for now we will use this mod.
+
+Stargate Teleporters Complete by Chris
+https://steamcommunity.com/sharedfiles/filedetails/?id=2933015619
+
+You can find the teleport configuration file customized for Deadfall at `extras/CBD_PortalTeleporterSystem.json`.  
+This file should be placed in your server profile directory to enable the teleporters.
+
+
+### GasMasksOnly (recommended)
+
+Deadfall has a large toxic zone at the passage.  It is recommended to use the GasMasksOnly to make it
+easier for players to get through this area.  Without this mod, you'll need to add the NBC gear to your
+economy via `db/types.xml`.
+
+GasMasksOnly by Crocodoc
+https://steamcommunity.com/sharedfiles/filedetails/?id=2621103156
+
+
+### SpawnerBubaku (server mod only)
+
+To spawn AI in many underground and difficult-to-place areas, there is an included configuration file at
+`extras/SpawnerBubaku/SpawnerBubaku.json`.
+
+SpawnerBubaku by Hunterz
+https://steamcommunity.com/sharedfiles/filedetails/?id=2482312670
---
+++ dayz.Deadfall/init.c
@@ -0,0 +1,95 @@
+void main()
+{
+	//INIT ECONOMY--------------------------------------
+	Hive ce = CreateHive();
+	if ( ce )
+		ce.InitOffline();
+
+	//Loot spawn Creator
+	//GetCEApi().ExportProxyData(vector.Zero, 100000); //Loot
+	//GetCEApi().ExportClusterData(); //Fruit
+
+	// fixed date
+	int year, month, day, hour, minute;
+	GetGame().GetWorld().GetDate( year, month, day, hour, minute );
+
+    if ( month != 10 )
+    {
+    	year = 2021;
+        month = 10;
+        day = 1;
+		
+		GetGame().GetWorld().SetDate( year, month, day, hour, minute );
+	}
+}
+
+class CustomMission: MissionServer
+{
+	void SetRandomHealth(EntityAI itemEnt)
+	{
+		if ( itemEnt )
+		{
+			float rndHlt = Math.RandomFloat( 0.45, 0.65 );
+			itemEnt.SetHealth01( "", "", rndHlt );
+		}
+	}
+
+	override PlayerBase CreateCharacter(PlayerIdentity identity, vector pos, ParamsReadContext ctx, string characterName)
+	{
+		Entity playerEnt;
+		playerEnt = GetGame().CreatePlayer( identity, characterName, pos, 0, "NONE" );
+		Class.CastTo( m_player, playerEnt );
+
+		GetGame().SelectPlayer( identity, m_player );
+
+		return m_player;
+	}
+
+	override void StartingEquipSetup(PlayerBase player, bool clothesChosen)
+	{
+		EntityAI itemClothing;
+		EntityAI itemEnt;
+		ItemBase itemBs;
+		float rand;
+
+		itemClothing = player.FindAttachmentBySlotName( "Body" );
+		if ( itemClothing )
+		{
+			SetRandomHealth( itemClothing );
+			
+			// rag
+			itemEnt = itemClothing.GetInventory().CreateInInventory( "Rag" );
+			if ( Class.CastTo( itemBs, itemEnt ) )
+			{
+				SetRandomHealth( itemEnt );
+				itemBs.SetQuantity( 4 );
+				itemBs.SetCleanness( 1 );
+			}
+			player.SetQuickBarEntityShortcut( itemEnt, 0 );
+
+			// food
+			rand = Math.RandomFloatInclusive( 0.0, 1.0 );
+			if ( rand < 0.35 )
+				itemEnt = player.GetInventory().CreateInInventory( "Apple" );
+			else if ( rand > 0.65 )
+				itemEnt = player.GetInventory().CreateInInventory( "Pear" );
+			else
+				itemEnt = player.GetInventory().CreateInInventory( "Plum" );
+			SetRandomHealth( itemEnt );
+
+			// map
+			//itemClothing.GetInventory().CreateInInventory( "ChernarusMap" );
+		}
+		
+		itemClothing = player.FindAttachmentBySlotName( "Legs" );
+		if ( itemClothing )
+			SetRandomHealth( itemClothing );
+		
+		itemClothing = player.FindAttachmentBySlotName( "Feet" );
+	}
+};
+
+Mission CreateCustomMission(string path)
+{
+	return new CustomMission();
+}
